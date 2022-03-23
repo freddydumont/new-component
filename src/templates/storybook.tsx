f215f85ead843db766e43c1ed0582ac2a865bf12
Frederick Morin <frederickmorin18@gmail.com>
2022-03-23 02:08:45 -0400

    Fix storybook template

--- src/templates/storybook.tsx
+++ src/templates/storybook.tsx
@@ -1,14 +1,14 @@
 import { expect } from '@storybook/jest';
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { within } from '@storybook/testing-library';
-import COMPONENT_NAME, { COMPONENT_NAMEProps } from './COMPONENT_NAME';
+import COMPONENT_NAME from './COMPONENT_NAME';
 
 export default {
   title: 'Components/COMPONENT_NAME',
   component: COMPONENT_NAME,
-} as ComponentMeta<COMPONENT_NAMEProps>;
+} as ComponentMeta<typeof COMPONENT_NAME>;
 
-const Template: ComponentStory<COMPONENT_NAMEProps> = (args) => <COMPONENT_NAME {...args} />;
+const Template: ComponentStory<typeof COMPONENT_NAME> = (args) => <COMPONENT_NAME {...args} />;
 
 export const Default = Template.bind({});
 
